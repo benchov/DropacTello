8fbe6a00c96edcc8b42af5267a002184b7382c30
Viktor Mihályi <viktor.mihalyi@yahoo.com>
2020-07-29 20:34:34 +0200

    feat(FunctionalButtonGroup.tsx): Initial add of new component

---
+++ components/FunctionalButtonGroup.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import CommandButton from './IconButton'
+
+import colors from '../style/colors';
+import size from '../style/size';
+
+interface FunctionalButtonGroupProps {
+    onClick: any,
+}
+
+const FunctionalButtonGroup: React.SFC<FunctionalButtonGroupProps> = (props) => {
+    const onClickHandler = (value: string) => {
+        props.onClick(value);
+    };
+    return (
+        <View style={styles.container}>
+            <CommandButton size={size.medium} onClick={onClickHandler} color={colors.yellowMedium} commandType="takeoff" />
+            <View>
+                <CommandButton size={size.large} onClick={onClickHandler} color={colors.yellowMedium} commandType="emergency" />
+            </View>
+            <CommandButton size={size.medium} onClick={onClickHandler} color={colors.yellowMedium} commandType="land" />
+        </View>
+    )
+}
+
+export default FunctionalButtonGroup
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: colors.backgroundLight,
+        // opacity: 0.9,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginTop: 3,
+        padding: 20,
+        borderColor: colors.yellowMedium,
+        borderWidth: 1,
+    }
+})
