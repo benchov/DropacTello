39541f7e53374a86a07491826ae2430301f2af6e
Viktor Mihályi <viktor.mihalyi@yahoo.com>
2020-07-29 20:29:07 +0200

    feat(colors.ts): initial add of new style related file

---
+++ style/colors.ts
@@ -0,0 +1,9 @@
+export default {
+    backgroundLight: '#414143',
+    backgroundDark: '#020202',
+    yellowLight: '#ffdf01',
+    yellowMedium: '#f8db4f',
+    yellowDark: '#fdb719',
+    white: '#f7f7f7',
+    black: '#0f0b0c',
+}
