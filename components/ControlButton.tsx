d23b7f6b24b8a3ed63e61d5ee3ef509f444d7cea
Viktor Mihályi <viktor.mihalyi@yahoo.com>
2020-09-08 13:02:44 +0200

    feat(ControlButton.tsx): Initial add of new component

---
+++ components/ControlButton.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import { StyleSheet, View, TouchableWithoutFeedback } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
+import colors from '../style/colors';
+import size from '../style/size';
+
+interface Props {
+    handlePressIn: any,
+    handlePressOut: any,
+    pressed: boolean,
+    iconDirection: string,
+    style?: object
+}
+const ControlButton: React.FC<Props> = (props) => {
+    const handlePress = (status: string) => {
+        if (status === 'in') {
+            props.handlePressIn()
+        }
+        if (status === 'out') {
+            props.handlePressOut()
+        }
+    }
+
+    return (
+        <React.Fragment>
+            <TouchableWithoutFeedback onPressIn={() => handlePress('in')} onPressOut={() => handlePress('out')}>
+                <View style={[props.style, props.pressed ? styles.pressFeedback : null]}>
+                    <Entypo name={`chevron-thin-${props.iconDirection}`} size={size.large} color={colors.yellowMedium} />
+                </View>
+            </TouchableWithoutFeedback>
+        </React.Fragment >
+    )
+}
+
+export default ControlButton;
+
+const styles = StyleSheet.create({
+    pressFeedback: {
+        backgroundColor: colors.backgroundLight,
+        borderColor: colors.backgroundLight,
+        borderWidth: 1,
+        borderRadius: 50
+    }
+})
