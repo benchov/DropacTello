536514c71b5a3cffedefa7c57af6d8495184b7bb
Viktor Mihályi <viktor.mihalyi@yahoo.com>
2021-01-17 17:40:56 +0100

    draft feat(test/App.js): Initial add of new test

--- __tests__/App.js
+++ __tests__/App.js
@@ -4,7 +4,12 @@ import App from '../App.tsx';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import { isExportDeclaration } from 'typescript';
 
 it('renders correctly', () => {
   renderer.create(<App />);
+});
+
+it('parsing state correctly', () => {
+
 });
