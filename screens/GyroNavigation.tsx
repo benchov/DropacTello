c9269bd678170257191ee9e302c6b57a5649aa91
Viktor Mihályi <viktor.mihalyi@yahoo.com>
2021-01-19 20:12:13 +0100

    feat(GyroNavigation): Add basic implementation of arrow feedback

--- screens/GyroNavigation.tsx
+++ screens/GyroNavigation.tsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform } from 'react-native';
 import { Gyroscope } from 'expo-sensors';
+import * as ScreenOrientation from 'expo-screen-orientation';
 import colors from '../style/colors';
-
+import ControlButton from '../components/ControlButton';
 interface Coordinates {
     x: number,
     y: number,
@@ -11,7 +12,7 @@ interface Coordinates {
 
 const GyroNavigation = () => {
     const [data, setData] = useState<Coordinates>({ x: 0, y: 0, z: 0 });
-    Gyroscope.setUpdateInterval(1000);
+    Gyroscope.setUpdateInterval(50);
 
 
     useEffect(() => {
@@ -20,22 +21,53 @@ const GyroNavigation = () => {
         });
     }, []);
 
-
-    const _subscribe = () => {
-        Gyroscope.addListener(gyroscopeData => {
-            setData(gyroscopeData);
-        });
-        Gyroscope.setUpdateInterval(16);
-    };
-
+    useEffect(() => {
+        if (Platform.OS !== 'web') {
+            ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT)
+        }
+    }, [])
 
     let { x, y, z } = data;
     return (
-        <View style={styles.sensor}>
-            <Text style={styles.text}>Gyroscope:</Text>
-            <Text style={styles.text}>
-                x: {round(x)} y: {round(y)} z: {round(z)}
-            </Text>
+        <View style={styles.mainContainer}>
+            <View style={styles.sensor}>
+                <Text style={styles.text}>Gyroscope:</Text>
+                <Text style={styles.text}>
+                    x: {round(x)} y: {round(y)} z: {round(z)}
+                </Text>
+            </View>
+            <View style={styles.container}>
+                <View style={styles.mainArrowContainer}>
+                    <ControlButton
+                        handlePressIn={() => console.log('object')}
+                        handlePressOut={() => console.log('object')}
+                        pressed={data.y < -2}
+                        iconDirection={"left"}
+                    />
+                    <View style={styles.upDownContainer}>
+                        <ControlButton
+                            handlePressIn={() => console.log('object')}
+                            handlePressOut={() => console.log('object')}
+                            pressed={data.x < -2}
+                            iconDirection={"up"}
+                            style={{ paddingBottom: 25 }}
+                        />
+                        <ControlButton
+                            handlePressIn={() => console.log('object')}
+                            handlePressOut={() => console.log('object')}
+                            pressed={data.x > 2}
+                            iconDirection={"down"}
+                            style={{ paddingTop: 25 }}
+                        />
+                    </View>
+                    <ControlButton
+                        handlePressIn={() => console.log('object')}
+                        handlePressOut={() => console.log('object')}
+                        pressed={data.y > 2}
+                        iconDirection={"right"}
+                    />
+                </View>
+            </View>
         </View>
     );
 }
@@ -57,6 +89,33 @@ const styles = StyleSheet.create({
     },
     text: {
         color: colors.yellowMedium
+    },
+    mainArrowContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 150
+    },
+    upDownContainer: {
+        backgroundColor: colors.backgroundDark,
+    },
+    container: {
+        flex: 4,
+        backgroundColor: colors.backgroundDark,
+        flexDirection: 'row',
+        alignItems: 'flex-start',
+        justifyContent: 'space-between',
+        marginTop: 3,
+        borderColor: colors.yellowMedium,
+        borderWidth: 1,
+        borderBottomLeftRadius: 60,
+        borderBottomRightRadius: 60,
+    },
+    mainContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around',
     }
 })
 export default GyroNavigation;
